4bd6ba7e6832083af238cb70b86440bda2b93c91
Shivkumar13 <sople@redhat.com>
2023-08-24 20:42:08 +0530

    First commit which has everything

---
+++ 4.12/tools.go
@@ -0,0 +1,18 @@
+//go:build tools
+// +build tools
+
+// Official workaround to track tool dependencies with go modules:
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+package tools
+
+import (
+	// dependency of hack/go-lint.sh
+	_ "golang.org/x/lint"
+
+	// dependency of generating CRD for install-config
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+
+	// used to generate mocks
+	_ "github.com/golang/mock/mockgen"
+)
